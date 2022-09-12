19ee02823ec5156b7bc62df3dc19feef34420867
Shawn <dominique599@pm.me>
2022-09-12 12:52:05 +0800

    initial setup...27:59 starting react navigation setup

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Deliveries</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <TailwindProvider>
+        <View style={styles.container}>
+          <Text className="text-red-500">Deliveries</Text>
+          <StatusBar style="auto" />
+        </View>
+      </TailwindProvider>
+    </NavigationContainer>
   );
 }
 
